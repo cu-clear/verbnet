8dc7d79a228763958c41bd1afbdb79d826ee21f8
Adam <ajwieme@gmail.com>
2017-06-29 13:56:59 -0500

    compare_members bugs, and update the output format

--- scripts/compare_versions.py
+++ scripts/compare_versions.py
@@ -22,7 +22,6 @@ def compare_members(from_vn_members, to_vn_members):
     possible_to_vn_members = search.find_members(to_vn_members, name=from_vn_member.name, wn=from_vn_member.wn)
     operations = []
     attr_diffs = None
-    all_changes.setdefault(from_vn_member.class_id(), {})
 
     # First case is that this member is in the same class in to_vn
     possible_match = [m for m in possible_to_vn_members if m.class_id() == from_vn_member.class_id()]
@@ -38,35 +37,33 @@ def compare_members(from_vn_members, to_vn_members):
       for possible_to_vn_member in possible_to_vn_members:
         if len(search.find_members(from_vn_members, class_ID=to_vn_member.class_id(), name=from_vn_member.name)) == 0:
           to_vn_member = possible_to_vn_member
-          operations.append(("move", to_vn_member.class_id()))
+          operations.append((from_vn_member.name[0], "move", to_vn_member.class_id()))
           # Compare the attributes
           attr_diffs = from_vn_member.compare_attrs(to_vn_member)
     elif len(possible_to_vn_members) == 1:
       to_vn_member = possible_to_vn_members[0]
-      operations.append(("move", to_vn_member.class_id()))
+      operations.append((from_vn_member.name[0], "move", to_vn_member.class_id()))
       # Compare the attributes
       attr_diffs = from_vn_member.compare_attrs(to_vn_member)
-
-      # Record the moved member as an insertion in that class
-      all_changes.setdefault(to_vn_member.class_id(), {})
-      all_changes[to_vn_member.class_id()] = {to_vn_member.name[0]: [("insert", to_vn_member.pp())]}
     else:
-      operations.append(("delete", None))
+      operations.append((from_vn_member.name[0], "delete", None))
 
     if attr_diffs:  # If member has updates to its attributes
-      operations.append(("update", attr_diffs))
+      operations.append((from_vn_member.name[0], "update", attr_diffs))
 
     if operations:
-      all_changes[from_vn_member.class_id()][from_vn_member.name[0]] = operations
+      all_changes[from_vn_member.class_id()] = operations
 
-  # Lastly, we have to get all to_vn_members that did not exist in
-  # from_vn_members to record all of the rest of the insert operations
+  '''
+    Lastly, we have to get all to_vn_members that did not exist in
+    from_vn_members to record all of the rest of the insert operations
+    Get string name in order to hash it for the set,
+    and then change it back to a list in order to work with search.find_members
+  '''
   for name, class_ID in list(set([(m.name[0], m.class_id()) for m in to_vn_members]) - set([(m.name[0], m.class_id()) for m in from_vn_members])):
-    # Have to get string name above, on order to hash it for set,
-    # and then change it back to a list in order to wok with search.find_members
     inserted_member = search.find_members(to_vn_members, class_ID=class_ID, name=[name])
     if inserted_member:
-      all_changes[inserted_member[0].class_id()] = {inserted_member[0].name[0]: [("insert", inserted_member[0].pp())]}
+      all_changes.setdefault(inserted_member[0].class_id(), []).append((inserted_member[0].name[0], "insert", inserted_member[0].pp()))
 
   return all_changes
 
