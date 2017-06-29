d1680afca52e5d5c8c440d7773ef7712c7ec5247
Adam <ajwieme@gmail.com>
2017-06-29 15:39:07 -0500

    begin themrole comparisons, and break each attr_diff into a seperate update

--- scripts/compare_versions.py
+++ scripts/compare_versions.py
@@ -11,9 +11,8 @@ def compare_members(from_vn_members, to_vn_members):
   '''
     update: member still in class, but attributes changed
     delete: member removed from class, and not moved to a new VN class
-    insert: member inserted to the class (right Now, it could be moved from another class
-            OR simply a new verb added to the lexicon)
-    move: member moved to a new vn class
+    insert: member inserted to the class, and is newly added to VN
+    move: member moved from the class to a new vn class
   '''
   all_changes = {}
 
@@ -49,7 +48,8 @@ def compare_members(from_vn_members, to_vn_members):
       operations.append((from_vn_member.name[0], "delete", None))
 
     if attr_diffs:  # If member has updates to its attributes
-      operations.append((from_vn_member.name[0], "update", attr_diffs))
+      for attr, diff in attr_diffs.items():
+        operations.append((from_vn_member.name[0], "update", {attr: diff}))
 
     if operations:
       all_changes[from_vn_member.class_id()] = operations
@@ -66,6 +66,67 @@ def compare_members(from_vn_members, to_vn_members):
       all_changes.setdefault(inserted_member[0].class_id(), []).append((inserted_member[0].name[0], "insert", inserted_member[0].pp()))
 
   return all_changes
+
+def compare_themroles(from_vn_themroles, to_vn_themroles):
+  '''
+    update: themrole still in class, but selectional restricyions changed
+    delete: themrole removed from class
+    insert: themrole inserted to the class
+  '''
+  all_changes = {}
+
+  for from_themrole in from_vn_themroles:
+    # Find that themrole in to_vn_themroles, and if there are differences, record them
+    possible_to_vn_themroles = search.find_themroles(to_vn_themroles, name=from_vn_member.name, wn=from_vn_member.wn)
+    operations = []
+    attr_diffs = None
+
+    # First case is that this member is in the same class in to_vn
+    possible_match = [m for m in possible_to_vn_members if m.class_id() == from_vn_member.class_id()]
+
+    if possible_match:  # If member is in the same class
+      to_vn_member = possible_match[0]
+      attr_diffs = from_vn_member.compare_attrs(to_vn_member)
+    # If there are many possible members, none of which are in the same class
+    elif len(possible_to_vn_members) > 1:
+      # Then try to see if any of those ALSO exist in from_vn
+      # This is to identify an instance of this member in a NEW class
+      # to say that this is where it moved to
+      for possible_to_vn_member in possible_to_vn_members:
+        if len(search.find_members(from_vn_members, class_ID=to_vn_member.class_id(), name=from_vn_member.name)) == 0:
+          to_vn_member = possible_to_vn_member
+          operations.append((from_vn_member.name[0], "move", to_vn_member.class_id()))
+          # Compare the attributes
+          attr_diffs = from_vn_member.compare_attrs(to_vn_member)
+    elif len(possible_to_vn_members) == 1:
+      to_vn_member = possible_to_vn_members[0]
+      operations.append((from_vn_member.name[0], "move", to_vn_member.class_id()))
+      # Compare the attributes
+      attr_diffs = from_vn_member.compare_attrs(to_vn_member)
+    else:
+      operations.append((from_vn_member.name[0], "delete", None))
+
+    if attr_diffs:  # If member has updates to its attributes
+      operations.append((from_vn_member.name[0], "update", attr_diffs))
+
+    if operations:
+      all_changes[from_vn_member.class_id()] = operations
+
+  '''
+    Lastly, we have to get all to_vn_members that did not exist in
+    from_vn_members to record all of the rest of the insert operations
+    Get string name in order to hash it for the set,
+    and then change it back to a list in order to work with search.find_members
+  '''
+  for name, class_ID in list(set([(m.name[0], m.class_id()) for m in to_vn_members]) - set(
+      [(m.name[0], m.class_id()) for m in from_vn_members])):
+    inserted_member = search.find_members(to_vn_members, class_ID=class_ID, name=[name])
+    if inserted_member:
+      all_changes.setdefault(inserted_member[0].class_id(), []).append(
+        (inserted_member[0].name[0], "insert", inserted_member[0].pp()))
+
+  return all_changes
+
 
 def compare_frames(from_vn_frames, to_vn_frames):
   '''
