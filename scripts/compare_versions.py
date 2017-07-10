5fc8ac154daff2368b83bda84f2f7e9496f724e1
Adam <ajwieme@gmail.com>
2017-07-10 13:30:04 -0600

    apply Change class to scripts

--- scripts/compare_versions.py
+++ scripts/compare_versions.py
@@ -37,6 +37,8 @@ class Change():
   
 def compare_members(from_vn_members, to_vn_members):
   '''
+    returns a dict of {to_vn_class: [Change objects]}
+
     update: member still in class, but attributes changed
     delete: member removed from class, and not moved to a new VN class
     insert: member inserted to the class, and is newly added to VN
@@ -47,7 +49,8 @@ def compare_members(from_vn_members, to_vn_members):
   for from_vn_member in from_vn_members:
     # Find that member in to_vn_members, and if there are differences, record them
     possible_to_vn_members = search.find_members(to_vn_members, name=from_vn_member.name, wn=from_vn_member.wn)
-    operations = []
+    changes = []
+    to_vn_member = None
     attr_diffs = None
 
     # First case is that this member is in the same class in to_vn
@@ -62,25 +65,24 @@ def compare_members(from_vn_members, to_vn_members):
       # This is to identify an instance of this member in a NEW class
       # to say that this is where it moved to
       for possible_to_vn_member in possible_to_vn_members:
-        if len(search.find_members(from_vn_members, class_ID=to_vn_member.class_id(), name=from_vn_member.name)) == 0:
+        if len(search.find_members(from_vn_members, class_ID=possible_to_vn_member.class_id(), name=from_vn_member.name)) == 0:
           to_vn_member = possible_to_vn_member
-          operations.append((from_vn_member.name[0], "move", to_vn_member.class_id()))
+          changes.append(Change(from_vn_member.name[0], "member", "move", from_vn_member.class_id()))
           # Compare the attributes
           attr_diffs = from_vn_member.compare_attrs(to_vn_member)
     elif len(possible_to_vn_members) == 1:
       to_vn_member = possible_to_vn_members[0]
-      operations.append((from_vn_member.name[0], "move", to_vn_member.class_id()))
+      changes.append(Change(from_vn_member.name[0], "member", "move", from_vn_member.class_id()))
       # Compare the attributes
       attr_diffs = from_vn_member.compare_attrs(to_vn_member)
     else:
-      operations.append((from_vn_member.name[0], "delete", None))
+      changes.append(Change(from_vn_member.name[0], "member", "delete", from_vn_member.class_id()))
 
     if attr_diffs:  # If member has updates to its attributes
-      for attr, diff in attr_diffs.items():
-        operations.append((from_vn_member.name[0], "update", {attr: diff}))
+      changes.append(Change(from_vn_member.name[0], "member", "update", from_vn_member.class_id(), ', '.join(["%s: %s" % (attr, diff) for attr, diff in attr_diffs.items()])))
 
-    if operations:
-      all_changes[from_vn_member.class_id()] = operations
+    if changes:
+      all_changes[to_vn_member.class_id() if to_vn_member else to_vn_member] = changes
 
   '''
     Lastly, we have to get all to_vn_members that did not exist in
@@ -91,12 +93,14 @@ def compare_members(from_vn_members, to_vn_members):
   for name, class_ID in list(set([(m.name[0], m.class_id()) for m in to_vn_members]) - set([(m.name[0], m.class_id()) for m in from_vn_members])):
     inserted_member = search.find_members(to_vn_members, class_ID=class_ID, name=[name])
     if inserted_member:
-      all_changes.setdefault(inserted_member[0].class_id(), []).append((inserted_member[0].name[0], "insert", inserted_member[0].pp()))
+      all_changes.setdefault(inserted_member[0].class_id(), []).append(Change(inserted_member[0].name[0], "member", "insert", notes=inserted_member[0].pp()))
 
   return all_changes
 
 def compare_themroles(from_vn_themroles, to_vn_themroles):
   '''
+    returns a dict of {to_vn_class: [Change objects]}
+
     update: themrole still in class, but selectional restricyions changed
     delete: themrole removed from class
     insert: themrole inserted to the class
@@ -112,9 +116,9 @@ def compare_themroles(from_vn_themroles, to_vn_themroles):
     # First case is that this member is in the same class in to_vn
     possible_match = [t for t in possible_to_themroles if t.class_id() == from_themrole.class_id()]
 
-    if possible_match:  # If member is in the same class
+    if possible_match:  # If themrole is in the same class
       to_vn_member = possible_match[0]
-      attr_diffs = from_vn_member.compare_attrs(to_vn_member)
+      attr_diffs = from_themrole.compare_attrs(to_vn_member)
     # If there are many possible members, none of which are in the same class
     elif len(possible_to_vn_members) > 1:
       # Then try to see if any of those ALSO exist in from_vn
@@ -230,9 +234,9 @@ from_vn_members = from_vn.get_all_members()
 to_vn_members = to_vn.get_all_members()
 
 x = compare_members(from_vn_members, to_vn_members)
-# Only print out classes that have changes in them
-for k, v in {k: v for k, v in x.items() if v}.items():
-  print({k: v})
+
+for k, v in x.items():
+  print({k: [change.__dict__ for change in v]})
 
 
 
