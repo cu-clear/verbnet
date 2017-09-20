f504c2d2c7996795ddaf4a43f2c4b13e8fe4c77d
Kevin Stowe <kevincstowe@gmail.com>
2017-09-19 18:48:53 -0600

    minor semnet format changes

--- semnet/semnet.py
+++ semnet/semnet.py
@@ -1,6 +1,9 @@
 import sys
 import getopt
 import json
+import itertools
+
+from csv import writer
 
 sys.path.append("../api/")
 
@@ -8,35 +11,110 @@ import verbnet
 import vnfn
 
 DEFAULT_VN_LOC = "../vn3.3.1-test/"
-DEFAULT_OUTPUT = "semnet-test.json"
+DEFAULT_OUTPUT = "semnet3"
+WN_LOCATION = "/home/kevin/Lexical_Resources/Wordnet/WordNet-3.0/dict/"
 
 
-def to_json_by_member(vn, output_file):
+all_senses = None
+verb_data= None
+
+def get_wn_synset(sense_key):
+    global all_senses, verb_data
+    if not all_senses:
+        all_senses = {sense.split()[0]:sense.split() for sense in open(WN_LOCATION + 'index.sense').readlines()}
+    if not verb_data:
+        verb_data = {verb.split()[0]:verb.split() for verb in open(WN_LOCATION + 'data.verb').readlines()}
+
+    if type(sense_key) == str:
+        sense_key = [sense_key]
+
+    synonyms = set()
+    for k in sense_key:
+        if k not in all_senses:
+            if k + "::" in all_senses:
+                k = k + "::"
+            else:
+                return None
+    
+        byte_offset = all_senses[k][1]
+
+        if byte_offset not in verb_data.keys():
+            return None
+        else:
+            words_in_synset = int(verb_data[byte_offset][3], 16)
+            synonyms.update([verb_data[byte_offset][w] for w in range(4, 4+(words_in_synset*2), 2)])
+    return synonyms
+
+                                                
+def build_semnet(vn):
     res = {}
-    mappings = vnfn.load_mappings("../api/vn-fn.s", as_dict=True)
+    mappings = vnfn.load_mappings("../api/semlink/vn-fn.s", as_dict=True)
 
     for cl in vn.get_verb_classes():
         new_id = cl.ID
         full_themroles = cl.themroles
+        restrictions = []
         while "-" in "-".join(new_id.split("-")[1:]):
             p_class = vn.verb_classes_dict["-".join(new_id.split("-")[:-1])]
             full_themroles.extend(p_class.themroles)
+            for r in full_themroles:
+                for thing in r.sel_restrictions:
+                    if type(thing) == list and len(thing) == 2 and "".join(thing) not in restrictions:
+                        restrictions.append("".join(thing))
+                        
             new_id = "-".join(new_id.split("-")[:-1])
 
         for member in cl.members:
-            if cl.frames:
-                norm_id = "-".join(cl.ID.split("-")[1:])
-                if member.name + ":" + norm_id not in mappings:
-                    mappings[member.name + ":" + norm_id] = ""
-                res[cl.ID + "-" + member.name] = {"wn": member.wn, "themroles": [str(r) for r in full_themroles],
-                                                  "syntactic_frames": [f.pp_syntax() for f in cl.frames],
-                                                  "semantic_frames": [f.pp_semantics() for f in cl.frames],
-                                                  "fn_frame":mappings[member.name + ":" + norm_id]}
+            norm_id = "-".join(cl.ID.split("-")[1:])
+            if member.name + ":" + norm_id not in mappings:
+                mappings[member.name + ":" + norm_id] = ""
 
-    with open(output_file, "w") as o:
-        json.dump(res, o)
-
+            member_data = {"wn": member.wn,
+                           "themroles": [r.role_type for r in full_themroles],
+                           "restrictions": restrictions,
+                           "frames": cl.frames,
+                           "fn_frame":mappings[member.name + ":" + norm_id],
+                           "wn_synset":get_wn_synset(member.wn)}
+            
+            if member.name in res:
+                res[member.name][cl.ID] = member_data
+            else:
+                res[member.name] = {cl.ID:member_data}
     return res
+
+def write_semnet(semnet, output_file, output_format):
+    for member in semnet.keys():
+        for vn_class in semnet[member]:
+            semnet[member][vn_class]["predicates"] = [p.value[0] for f in semnet[member][vn_class]["frames"] for p in f.predicates]
+            semnet[member][vn_class].pop("frames", None) 
+            semnet[member][vn_class]["themroles"] = semnet[member][vn_class]["themroles"]
+
+            print (semnet[member][vn_class].keys())
+            
+            for component in semnet[member][vn_class].keys():
+                if component == "restrictions":
+                    #TODO : Write restrictions however Martin wants 
+                    pass
+                elif component == "fn_frame":
+                    pass
+                else:
+                    try:
+                        semnet[member][vn_class][component] = " ".join(semnet[member][vn_class][component])
+                    except:
+                        pass
+                
+    if output_format == "json":
+        with open(output_file, 'w') as output:
+            json.dump(semnet, output)
+    elif output_format == "csv":
+        with open(output_file, 'w') as output:
+            output_writer = writer(output, delimiter=';')
+
+            for member in sorted(list(semnet.keys())):
+                for vn_class in sorted(list(semnet[member].keys())):
+                    data = [member, vn_class] + [semnet[member][vn_class][item] for item in sorted(list(semnet[member][vn_class].keys()))]
+                    output_writer.writerow(data)
+    return None
 
 
 def print_help():
@@ -48,7 +126,7 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
     try:
-        opts, args = getopt.getopt(argv[1:], "hi:o:", ["help", "input=", "output="])
+        opts, args = getopt.getopt(argv[1:], "hi:o:j", ["help", "input=", "output=", "json"])
     except Exception as e:
         print(e)
         print("Error in args : " + str(argv[1:]))
@@ -64,12 +142,15 @@ def main(argv=None):
             output_file = o[1]
         if o[0] in ["-h", "--help"]:
             print_help()
-
+        if o[0] in ["-j", "--json"]:
+            generate_json = True
+            
     print('input dir    :  ' + input_dir)
     print('writing to   :  ' + output_file)
 
-    to_json_by_member(verbnet.VerbNetParser(directory=input_dir), output_file)
-
+    sn = build_semnet(verbnet.VerbNetParser(directory=input_dir))
+#    write_semnet(sn, output_file+".json", "json")
+    write_semnet(sn, output_file+".csv", "csv")
 
 if __name__ == "__main__":
     sys.exit(main())
