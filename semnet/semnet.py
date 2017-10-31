bab2a0d45d47544ab0686b89955d268b45e344b9
Kevin Stowe <kevincstowe@gmail.com>
2017-10-30 18:46:06 -0600

    updated semnet to include common arguments from James Gung's work

--- semnet/semnet.py
+++ semnet/semnet.py
@@ -13,7 +13,7 @@ import vnfn
 DEFAULT_VN_LOC = "../vn3.3.1-test/"
 DEFAULT_OUTPUT = "semnet3"
 WN_LOCATION = "/home/kevin/Lexical_Resources/Wordnet/WordNet-3.0/dict/"
-VN_OBJECTS_LOCATION = "../../vn_versions/vn_objects/"
+VN_OBJECTS_LOCATION = "../vn_versions/vn_objects/"
 
 all_senses = None
 verb_data= None
@@ -81,11 +81,11 @@ def build_semnet(vn):
                 mappings[member.name + ":" + norm_id] = ""
 
             member_data = {"wn": member.wn,
-                           "themroles": [r.role_type for r in full_themroles],
+                           "themroles":[r.role_type for r in full_themroles],
                            "restrictions": restrictions,
                            "frames": cl.frames,
                            "fn_frame":mappings[member.name + ":" + norm_id],
-                           #"wn_synset":get_wn_synset(member.wn),
+                           "wn_synset":get_wn_synset(member.wn),
                            "common_objects":get_vn_objects(member, cl)}
             
             if member.name in res:
@@ -99,7 +99,7 @@ def write_semnet(semnet, output_file, output_format):
         for vn_class in semnet[member]:
             semnet[member][vn_class]["predicates"] = set([p.value[0] for f in semnet[member][vn_class]["frames"] for p in f.predicates])
             semnet[member][vn_class].pop("frames", None) 
-            semnet[member][vn_class]["themroles"] = semnet[member][vn_class]["themroles"]
+            semnet[member][vn_class]["themroles"] = list(set(semnet[member][vn_class]["themroles"]))
             
             for component in semnet[member][vn_class].keys():
                 if component == "restrictions":
@@ -123,8 +123,13 @@ def write_semnet(semnet, output_file, output_format):
         with open(output_file, 'w') as output:
             output_writer = writer(output, delimiter=';')
 
+            header=False
             for member in sorted(list(semnet.keys())):
                 for vn_class in sorted(list(semnet[member].keys())):
+                    if not header:
+                        output_writer.writerow(["verb","class_name","class_id"] + sorted(list(semnet[member][vn_class].keys())))
+                        header=True
+                                                                                
                     data = [member, vn_class.split("-")[0], "-".join(vn_class.split("-")[1:])] + [semnet[member][vn_class][item] for item in sorted(list(semnet[member][vn_class].keys()))]
                     output_writer.writerow(data)
     return None
